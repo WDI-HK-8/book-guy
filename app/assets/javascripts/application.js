cc61ed0cb7a252f2c8506445cc2a6ea92cb9f68f
Richard Li <xpp@me.com>
2015-09-08 12:54:12 +0800

    feat(initialise-devise): user can now signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require angular/angular
 //= require angular-route/angular-route
+//= require ng-token-auth/dist/ng-token-auth
+//= require angular-cookie/angular-cookie
 //= require_tree .
